34d108fff106e69fed48828dae3d2e3e03dad5d7
Suabsh Dhami <110603740+gitsubash44@users.noreply.github.com>
2024-07-29 20:22:41 +0545

    first commit

---
+++ lib/nav_bar_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        elevation: 1,
+        height: 60.0,
+        color: Colors.white,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.grid_view_outlined),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
